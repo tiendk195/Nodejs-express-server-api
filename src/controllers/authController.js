8f23695d8bed8a67b71a7dfd66485d43359f7193
tiendk195 <tiendk195@gmail.com>
2024-03-30 17:13:02 +0700

    Update email valiate - password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,19 +1,41 @@
 import User from "../models/userModel";
-import { registerValidate } from "../validations/auth";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { registerValidator, loginValidator } from "../validations/auth";
 class AuthController {
   //POST Method :auth/login
   login(req, res) {}
   //POST Method :auth/register
   async register(req, res) {
     try {
-      //validate : email,password,username
-      const error = registerValidate.validate(req.body, {
-        abortEarly: false,
+      // user_id
+      //B1: validate: email, password, username
+      const { email, username, avatar, password } = req.body;
+      const { error } = registerValidator.validate(req.body);
+      if (error) {
+        const errors = error.details.map((err) => err.message);
+        return res.status(400).json({
+          message: errors,
+        });
+      }
+      // b2: validate email exitsing
+      const emailExist = await User.findOne({ email });
+      if (emailExist) {
+        return res.status(400).json({ message: "Email already exists" });
+      }
+      // b3 ma hoa password
+      const hashPassword = await bcryptjs.hash(password, 10);
+      // update db
+      const user = await User.create({
+        email,
+        username,
+        avatar,
+        password: hashPassword,
       });
-      console.log(error);
+      // b4 remove password in res
       res.status(200).json({
         message: "Register successfully",
-        // data: newUser,
+        data: { ...user.toObject(), password: undefined },
       });
     } catch (error) {
       res.status(400).json({
